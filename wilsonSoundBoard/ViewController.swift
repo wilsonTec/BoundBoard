0bb68cc3b66643f8a88b8bde0af60a2fde58a32d
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-10-24 13:42:27 -0500

    Initial Commit

---
+++ wilsonSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wilsonSoundBoard
+//
+//  Created by Mac 18 on 24/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
